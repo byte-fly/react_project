16b6023ee88f839d42bc3685c41dc4a40d1d25aa
My-GitHub123 <1577628224@qq.com>
2021-07-12 13:04:23 +0800

    update_redux02

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App.jsx'
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter> ,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<App/>,document.getElementById('root'))
